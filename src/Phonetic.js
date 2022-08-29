a4f75e6216d5c9920fcb38cc0a4eb4f3809c6ff8
alexandravelascosvoboda <alex.v.s@live.com>
2022-08-30 09:00:58 +1000

    Updated Phonetics for push

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank">
-        Listen
+        Listen here!
       </a>
       <br />
       {props.phonetic.text}
