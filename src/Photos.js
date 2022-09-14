a0ad3423cf55aef4f17e532e57fe702ac200ad6a
alexandravelascosvoboda <alex.v.s@live.com>
2022-09-14 13:34:09 +1000

    alt prop added

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,12 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="noreferrer"
+                  alt="this is a picture of the word"
+                >
                   <img src={photo.src.landscape} className="img-fluid" />
                 </a>
               </div>
