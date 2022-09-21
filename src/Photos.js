b8351f3faeafd4b57ab53f308f35882d1cba33fa
alexandravelascosvoboda <alex.v.s@live.com>
2022-09-22 09:46:00 +1000

    alt prop updated again

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="this is a picture of the word"
+                    alt="the word representation"
                   />
                 </a>
               </div>
